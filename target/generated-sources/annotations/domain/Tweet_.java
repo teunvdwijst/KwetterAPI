0510dd4226486610a95f5de07bb0990aa862b66c
Teun van der Wijst <t.vanderwijst@student.fontys.nl>
2018-03-02 15:40:20 +0100

    Release V2.4.1 updated toString functions of domain classes for JSON communication. NOT WORKING YET, Account working properly, Tweet not passing all parameters

--- target/generated-sources/annotations/domain/Tweet_.java
+++ target/generated-sources/annotations/domain/Tweet_.java
@@ -8,7 +8,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-03-02T14:50:08")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-03-02T15:31:10")
 @StaticMetamodel(Tweet.class)
 public class Tweet_ { 
 
